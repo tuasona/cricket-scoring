cc9c570e97fbd55fa5fa61d8770acf44e01e613d
tuasona <36388495+tuasona@users.noreply.github.com>
2018-02-12 13:34:31 +1300

    Added '-1' to 'scorecard.length'
    3rd line of code

--- web/js/cricketCalc.js
+++ web/js/cricketCalc.js
@@ -14,12 +14,12 @@ function enter(key) {
 }
 
 /**
-    Removes the last entry from  the scorecard, and subtracts it from the total score.
+    Removes the last entry from the scorecard, and subtracts it from the total score.
 */
 function backspace() {
     var previousBall = scorecard.charAt(scorecard.length - 1);
     score = score - previousBall;
-    scorecard = scorecard.substring(0, scorecard.length);
+    scorecard = scorecard.substring(0, scorecard.length - 1);
     document.getElementById('scorecard').innerHTML = scorecard;
     document.getElementById('score').innerHTML = score;
 }
